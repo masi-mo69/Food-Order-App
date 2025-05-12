d8c82d1bd68a0dc6f1e81a235ee11bbb6a6c4067
Mohammadi <ms.mohammadi69@gmail.com>
2025-05-12 19:58:55 +0330

    Adding Http & Forms To The Food Order App

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,13 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+    const [isCheckout , setIsCheckOut] = useState(false);
     const cartCtx = useContext(CartContext);
     
-    // تبدیل قیمت کل به تومان (با نرخ دلار 50,000 تومان)
+    // Convert the total price to Tomans (at a dollar rate of 50,000 Tomans)
     const exchangeRate = 50000;
     const totalInTomans = cartCtx.totalAmount * exchangeRate;
     const formattedTotal = totalInTomans.toLocaleString('fa-IR') + ' تومان';
@@ -23,7 +25,21 @@ const Cart = (props) => {
             ...item,
             amount: 1
         });
-    }
+    };
+
+    const orderHandler = () => {
+        setIsCheckOut(true);
+    };
+
+    const submitOrderHandler = (userData) => {
+        fetch('https://react-http-ef6d7-default-rtdb.firebaseio.com/orders.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        })
+    };
 
     const cartItems = 
         <ul className={classes['cart-items']}>
@@ -32,12 +48,25 @@ const Cart = (props) => {
                     key={item.id}
                     name={item.name}
                     amount={item.amount}
-                    price={item.price * exchangeRate} // ارسال قیمت به تومان
+                    price={item.price * exchangeRate} // Send Price to Tomans
                     onRemove={cartItemRemoveHandler.bind(null, item.id)}
                     onAdd={cartItemAddHandler.bind(null, item)}
                 />
             ))}
         </ul>;
+
+        const modalActions = (
+            <div className={classes.actions}>
+                <button className={classes['button--alt']} onClick={props.onClose}>
+                    بستن
+                </button>
+                {hasItems && (
+                    <button className={classes.button} onClick={orderHandler}>
+                        سفارش
+                    </button>
+                )}
+            </div>
+        );
 
     return (
         <Modal onClose={props.onClose}>
@@ -46,10 +75,13 @@ const Cart = (props) => {
                 <span>مجموع کل</span>
                 <span>{formattedTotal}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onClose}>بستن</button>
-                {hasItems && <button className={classes.button}>سفارش</button>}
-            </div>
+            { isCheckout && 
+            <Checkout 
+                onConfirm={submitOrderHandler} 
+                onCancel={props.onClose}
+            />
+            }
+            { !isCheckout && modalActions }
         </Modal>
     );
 }
